ee9ccaad7d1c47a9757000175c57826b99fcc613
hamza <shadeau99@gmail.com>
2020-04-20 19:35:02 +0100

    some modification too

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { Cards, CountryPicker, Chart } from './components';
+import { Cards, CountryPicker, Chart, Table } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import cx from 'classnames';
 
 import image from './images/image.png';
 
@@ -33,6 +35,24 @@ class App extends React.Component {
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
+        <h1 className={styles.titre}> بعض النصائح لمواجهة كورونا</h1>
+<Grid container spacing={3} justify="center" className={cx(styles.card, styles.recovered)}>
+<div className={styles.conseil} >
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-ready-social-3676759384ed44cc394abade6fd6d1eef.jpg?sfvrsn=1706a18f_12' alt="COVID-19" />
+    </div><div className={styles.conseil}> 
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-smart-if-you-develop-ar.jpg?sfvrsn=1486258a_11' alt="COVID-19" />
+    </div><div className={styles.conseil}>
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-smart-inform-ar.jpg?sfvrsn=f6dbe358_12' alt="COVID-19" />
+    </div><div className={styles.conseil}>
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-safe-ar.jpg?sfvrsn=1f6e4aef_12' alt="COVID-19" />
+    </div><div className={styles.conseil}>
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-kind-to-support-ar.jpg?sfvrsn=1856f2a3_14' alt="COVID-19" />
+    </div><div className={styles.conseil}>
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-kind-to-address-stigma-ar.jpg?sfvrsn=4615bfbe_12' alt="COVID-19" />
+    </div><div className={styles.conseil}>
+    <img className={styles.image} src='https://www.who.int/images/default-source/health-topics/coronavirus/social-media-squares/be-kind-to-address-fear-ar.jpg?sfvrsn=a8e99f14_12' alt="COVID-19" />
+</div>
+  </Grid>
       </div>
     );
   }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -16,7 +16,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              Infected
+              المصابين
             </Typography>
             <Typography variant="h5" component="h2">
               <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
@@ -25,14 +25,14 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               {new Date(lastUpdate).toDateString()}
             </Typography>
             <Typography variant="body2" component="p">
-              Number of active cases of COVID-19.
+                عدد المصابين بفيروس كوفيد 19 
             </Typography>
           </CardContent>
         </Grid>
         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              Recovered
+              المتعافين
             </Typography>
             <Typography variant="h5" component="h2">
               <CountUp start={0} end={recovered.value} duration={2.75} separator="," />
@@ -41,14 +41,14 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               {new Date(lastUpdate).toDateString()}
             </Typography>
             <Typography variant="body2" component="p">
-              Number of recoveries from COVID-19.
+                عدد المتعافين من كوفيد 19
             </Typography>
           </CardContent>
         </Grid>
         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.deaths)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              Deaths
+              الوفيات
             </Typography>
             <Typography variant="h5" component="h2">
               <CountUp start={0} end={deaths.value} duration={2.75} separator="," />
@@ -57,7 +57,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               {new Date(lastUpdate).toDateString()}
             </Typography>
             <Typography variant="body2" component="p">
-              Number of deaths caused by COVID-19.
+                 عدد الوفيات بفيروس 
             </Typography>
           </CardContent>
         </Grid>
